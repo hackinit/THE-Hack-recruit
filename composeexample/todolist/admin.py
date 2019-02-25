574d9b6dce3dfa0aa38ede8f9a255b8e49514922
Juncheng Zhu <zhujuncheng1021@163.com>
2019-02-25 11:29:29 +0800

    1.0.0(runnable)

---
+++ composeexample/todolist/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . import models
+
+# Register your models here.
+class TodoListAdmin(admin.ModelAdmin):
+    list_display = ("title",  "created", "due_date")
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+admin.site.register(models.TodoList, TodoListAdmin)
+admin.site.register(models.Category, CategoryAdmin)
---
+++ composeexample/todolist/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+class Category(models.Model): # The Category table name that inherits models.Model
+    name = models.CharField(max_length=100) #Like a varchar
+    class Meta:
+        verbose_name = ("Category")
+        verbose_name_plural = ("Categories")
+    def __str__(self):
+        return self.name #name to be shown when called
+
+class TodoList(models.Model): #Todolist able name that inherits models.Model
+    title = models.CharField(max_length=250) # a varchar
+    content = models.TextField(blank=True) # a text field 
+    created = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
+    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d")) # a date
+    category = models.ForeignKey(Category, default="general",on_delete=models.DO_NOTHING) # a foreignkey
+    class Meta:
+        ordering = ["-created"] #ordering by the created field
+    def __str__(self):
+        return self.title #name to be shown when called
